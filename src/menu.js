ba9fc72faf0ce2e836946ccc3d5ee7c56bc75e16
RTSolutionsLarry <larry.randolph@randolphtechnologysolutions.com>
2025-12-13 17:58:38 -0800

    Complete appetizer section

--- src/menu.js
+++ src/menu.js
@@ -35,12 +35,21 @@ const displayMenu = () => {
     ];
     
     const appetizerItems = menuItems.filter((item) => item.type = 'appetizer');
+    const appetizerItemsElements = [];
     const entreeItems = menuItems.filter((item) => item.type = 'entree');
+    const entreeItemsElements = [];
     const drinkItems = menuItems.filter((item) => item.type = 'drink');
+    const drinkItemsElements = [];
 
     //Appetizer Section
 
     const appetizerContainer = document.createElement('div');
+
+    const appetizerHeader = document.createElement('div');
+    appetizerHeader.classList.add('appetizerHeader');
+    appetizerHeader.innerText = 'Appetizers';
+    appetizerContainer.appendChild(appetizerHeader)
+
     appetizerContainer.classList.add('menuSection');
     appetizerContainer.classList.add('appetizer');
     const appetizerTop = document.createElement('div');
@@ -49,6 +58,7 @@ const displayMenu = () => {
         const appetizerItem = document.createElement('div');
         appetizerItem.classList.add('appetizerItem')
         appetizerTop.appendChild(appetizerItem);
+        appetizerItemsElements.push(appetizerItem)
     }
     const appetizerBottom = document.createElement('div');
     appetizerBottom.classList.add('appetizerBottom');    
@@ -56,20 +66,48 @@ const displayMenu = () => {
         const appetizerItem = document.createElement('div');
         appetizerItem.classList.add('appetizerItem')
         appetizerBottom.appendChild(appetizerItem);
+        appetizerItemsElements.push(appetizerItem)
+    }
+
+    let appetizerItemIndex = 0;
+    for (let element of appetizerItemsElements) {
+        const appetizerName = document.createElement('p');
+        const appetizerPrice = document.createElement('p');
+        appetizerName.classList.add('appetizerName');
+        appetizerName.innerText = appetizerItems[appetizerItemIndex].name;
+        appetizerPrice.classList.add('appetizerPrice');
+        appetizerPrice.innerText = appetizerItems[appetizerItemIndex].price;
+        element.appendChild(appetizerName);
+        element.appendChild(appetizerPrice);
+        appetizerItemIndex++;
     }
 
     //entree Section
 
     const entreeContainer = document.createElement('div');
+
+    const entreeHeader = document.createElement('div');
+    entreeHeader.classList.add('entreeHeader');
+    entreeHeader.innerText = 'Entrees';
+    entreeContainer.appendChild(entreeHeader);
+    
+    const entreeSection = document.createElement('div');
+    entreeSection.classList.add("entreeSection");
+    entreeContainer.appendChild(entreeSection);
+
     entreeContainer.classList.add('menuSection')
     const drinkContainer = document.createElement('div');
     drinkContainer.classList.add('menuSection')
 
     mainContent.appendChild(menuContainer);
     menuContainer.appendChild(appetizerContainer);
+
     appetizerContainer.appendChild(appetizerTop);
     appetizerContainer.appendChild(appetizerBottom);
+
     menuContainer.appendChild(entreeContainer);
+
+
     menuContainer.appendChild(drinkContainer);        
 }
 
