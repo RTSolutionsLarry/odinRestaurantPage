fd703e4dc361635735e547ca48237afb55560e02
RTSolutionsLarry <larry.randolph@randolphtechnologysolutions.com>
2026-01-09 13:05:36 -0800

    Main offer setup. Video background needs work.

--- src/mainOffer.js
+++ src/mainOffer.js
@@ -1,7 +1,18 @@
 const displayMainOffer = () => {
     console.log('Inside Main Content');
 
+    const mainContent = document.getElementsByClassName('mainSection')[0];
+
+    const mainOfferContainer = document.createElement('div');
+    mainOfferContainer.classList.add('mainOfferContainer');  
     
+    const videoBackground = document.createElement('video');
+    videoBackground.src = 'https://dam.tacobell.com/asset/13c214ee-cac4-4b72-9876-63097ca7f796/1080p_High/E126_NAT_CDB_Digital_VIDEO_web-desktop-1-mp4.mp4';
+    videoBackground.autoplay = true;
+    
+    mainContent.appendChild(mainOfferContainer);
+    mainOfferContainer.appendChild(videoBackground);
+
 }
 
 export {displayMainOffer};
