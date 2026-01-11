17a05d5b71307788d95ffc730f142ecda929cebb
RTSolutionsLarry <larry.randolph@randolphtechnologysolutions.com>
2026-01-11 11:29:00 -0800

    Video is fixed, add overlay text for offers

--- src/mainOffer.js
+++ src/mainOffer.js
@@ -9,9 +9,14 @@ const displayMainOffer = () => {
     const videoBackground = document.createElement('video');
     videoBackground.src = 'https://dam.tacobell.com/asset/13c214ee-cac4-4b72-9876-63097ca7f796/1080p_High/E126_NAT_CDB_Digital_VIDEO_web-desktop-1-mp4.mp4';
     videoBackground.autoplay = true;
+    videoBackground.load();
+    videoBackground.loop = true;
+    videoBackground.muted = true;
     
     mainContent.appendChild(mainOfferContainer);
     mainOfferContainer.appendChild(videoBackground);
+
+    //Make overlay div
 
 }
 
